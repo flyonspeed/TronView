86d14673aef368be65b3ebfae0a6cc2783ef0bf2
dinglewanker <iamsomebody@gmail.com>
2018-11-01 05:58:33 +0000

    show dashed lines below surface

--- hud.py
+++ hud.py
@@ -218,6 +218,38 @@ def readMessage():
   except serial.serialutil.SerialException:
     print "exception"
 
+class Point:
+    # constructed using a normal tupple
+    def __init__(self, point_t = (0,0)):
+        self.x = float(point_t[0])
+        self.y = float(point_t[1])
+    # define all useful operators
+    def __add__(self, other):
+        return Point((self.x + other.x, self.y + other.y))
+    def __sub__(self, other):
+        return Point((self.x - other.x, self.y - other.y))
+    def __mul__(self, scalar):
+        return Point((self.x*scalar, self.y*scalar))
+    def __div__(self, scalar):
+        return Point((self.x/scalar, self.y/scalar))
+    def __len__(self):
+        return int(math.sqrt(self.x**2 + self.y**2))
+    # get back values in original tuple format
+    def get(self):
+        return (self.x, self.y)
+
+def draw_dashed_line(surf, color, start_pos, end_pos, width=1, dash_length=10):
+    origin = Point(start_pos)
+    target = Point(end_pos)
+    displacement = target - origin
+    length = len(displacement)
+    slope = displacement/length
+
+    for index in range(0, length/dash_length, 2):
+        start = origin + (slope *    index    * dash_length)
+        end   = origin + (slope * (index + 1) * dash_length)
+        pygame.draw.line(surf, color, start.get(), end.get(), width)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -309,7 +341,10 @@ def main():
             #debug_print(args.debug, "Deg: {0}".format(l), 2)
             #debug_print(args.debug, "Line Coords: {0}".format(line_coords), 2)
             #debug_print(args.debug, "", 2)
-            pygame.draw.lines(ahrs_bg, WHITE, False, line_coords, line_thickness)
+            if(l<0):
+                draw_dashed_line(ahrs_bg, WHITE, line_coords[0], line_coords[1], width=line_thickness, dash_length=5)
+            else:
+                pygame.draw.lines(ahrs_bg, WHITE, False, line_coords, line_thickness)
 
             # render debug text
             #if show_debug:

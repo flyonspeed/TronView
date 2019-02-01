a6bf9a212b052c77ef14d00d4fbb053b820d4d8f
Brian Chesteen <brianchesteen@gmail.com>
2019-01-31 20:17:11 -0500

    Update serial_g3x.py
    Optimized TAS algorithm and added support for second sentence type which includes GPS Height AGL.

--- lib/inputs/serial_g3x.py
+++ lib/inputs/serial_g3x.py
@@ -3,12 +3,13 @@
 # Serial input source
 # Garmin G3X
 # 01/30/2019 Brian Chesteen, credit to Christopher Jones for developing template for input modules.
-from __future__ import print_function
 
+from __future__ import print_function
 from _input import Input
 from lib import hud_utils
 import serial
 import struct
+import math
 
 
 class serial_g3x(Input):
@@ -43,32 +44,43 @@ class serial_g3x(Input):
                 t = self.ser.read(1)
                 if len(t) != 0:
                     x = ord(t)
-            msg = self.ser.read(58)  
-            if len(msg) == 58:
-                msg = (msg[:58]) if len(msg) > 58 else msg
-                SentID, SentVer, UTCHour, UTCMin, UTCSec, UTCSecFrac, Pitch, Roll, Heading, Airspeed, PressAlt, RateofTurn, LatAcc, VertAcc, AOA, VertSpeed, OAT, AltSet, Checksum, CRLF = struct.unpack(
-                "cc2s2s2s2s4s5s3s4s6s4s3s3s2s4s3s3s2s2s", msg
-                )
-                # if ord(CRLF[0]) == 13:
-                if SentID == "1" and ord(CRLF[0]) == 13:
-                    aircraft.roll = int(Roll) * 0.1
-                    aircraft.pitch = int(Pitch) * 0.1
-                    aircraft.ias = int(Airspeed) * 0.1
-                    aircraft.PALT = int(PressAlt) 
-                    aircraft.aoa = int(AOA)
-                    aircraft.mag_head = int(Heading)
-                    aircraft.baro = (int(AltSet) + 2750.0) / 100
-                    aircraft.baro_diff = aircraft.baro - 29.921
-                    aircraft.alt = int(
-                        int(PressAlt) + (aircraft.baro_diff / 0.00108)
-                    )  # 0.00108 of inches of mercury change per foot.
-                    aircraft.BALT = aircraft.alt
-                    aircraft.vsi = int(VertSpeed) * 10
-                    aircraft.tas = ((aircraft.ias * 0.02) * (aircraft.BALT / 1000)) + aircraft.ias
-                    aircraft.msg_count += 1
+            SentID = self.ser.read(1)
+            
+            if SentID == "1": #atittude/air data message
+                msg = self.ser.read(57)  
+                if len(msg) == 57:
+                    msg = (msg[:57]) if len(msg) > 57 else msg
+                    SentVer, UTCHour, UTCMin, UTCSec, UTCSecFrac, Pitch, Roll, Heading, Airspeed, PressAlt, RateofTurn, LatAcc, VertAcc, AOA, VertSpeed, OAT, AltSet, Checksum, CRLF = struct.unpack(
+                    "c2s2s2s2s4s5s3s4s6s4s3s3s2s4s3s3s2s2s", msg
+                    )
+                    if SentVer == "1" and ord(CRLF[0]) == 13:
+                        aircraft.roll = int(Roll) * 0.1
+                        aircraft.pitch = int(Pitch) * 0.1
+                        aircraft.ias = int(Airspeed) * 0.1
+                        aircraft.PALT = int(PressAlt)
+                        aircraft.oat = int(OAT)
+                        aircraft.aoa = int(AOA)
+                        aircraft.mag_head = int(Heading)
+                        aircraft.baro = (int(AltSet) + 2750.0) / 100.0
+                        aircraft.baro_diff = aircraft.baro - 29.9213
+                        aircraft.alt = int(
+                            int(PressAlt) + (aircraft.baro_diff / 0.00108)
+                        )  # 0.00108 of inches of mercury change per foot.
+                        aircraft.BALT = aircraft.alt
+                        aircraft.vsi = int(VertSpeed) * 10
+                        aircraft.tas = aircraft.ias * (math.sqrt((273.0 + aircraft.oat) / 288.0)) * ((1.0 - aircraft.PALT / 144000.0) ** -2.75)
+                        aircraft.msg_count += 1
 
-                    self.ser.flushInput()
-                    return aircraft
+            elif SentID == "7": #GPS AGL data message
+                msg = self.ser.read(16)  
+                if len(msg) == 16:
+                    msg = (msg[:16]) if len(msg) > 16 else msg
+                    SentVer, UTCHour, UTCMin, UTCSec, UTCSecFrac, HeightAGL, Checksum, CRLF = struct.unpack(
+                    "c2s2s2s2s3s2s2s", msg
+                    )
+                    if SentVer == "1" and ord(CRLF[0]) == 13:
+                        aircraft.agl = int(HeightAGL) * 100
+                        aircraft.msg_count += 1
 
             else:
                 self.ser.flushInput()

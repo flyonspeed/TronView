ab11b9e4d9800ad518ddb2c2d6c0551cf7f790eb
Brantel <brianchesteen@gmail.com>
2019-02-11 21:57:04 -0500

    tweaked input modules to add additional parsing

--- lib/aircraft.py
+++ lib/aircraft.py
@@ -15,6 +15,7 @@ class Aircraft(object):
         self.agl = 0
         self.PALT = 0
         self.BALT = 0
+        self.DA = None
         self.aoa = 0
         self.mag_head = 0
         self.gndtrack = 0 #TODO: Move to GPSData class?
@@ -23,8 +24,8 @@ class Aircraft(object):
         self.vsi = 0
         self.gndspeed = 0 #TODO: Move to GPSData class?
         self.oat = 0
-        self.vert_G = 0
-        self.turn_rate = 0
+        self.vert_G = None
+        self.turn_rate = None
         self.wind_speed = None
         self.wind_dir = None
         self.norm_wind_dir = None #corrected for aircraft heading for wind direction pointer TODO: Add wind direction pointer to HUD screen

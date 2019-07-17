e4b3839e3bdd17854a79a63421b37d6eab704fc3
Dinglewanker <iamsomebody@gmail.com>
2019-07-17 02:42:53 +0000

    install pyhon audio lib

--- util/setup.sh
+++ util/setup.sh
@@ -6,7 +6,7 @@ case $yn in
 	[Yy]* )echo "Enabling serial port"
 		sudo bash -c 'echo " " >> /boot/config.txt'
 		sudo bash -c 'echo "enable_uart=1" >> /boot/config.txt'
-		sudo apt-get -y install python python-serial python-pygame
+		sudo apt-get -y install python python-serial python-pygame python-pyaudio
 		echo "Please reboot your pi now.  Type reboot"
 		;;
 	[Nn]* )echo "Ok. Nothing done."; exit;;

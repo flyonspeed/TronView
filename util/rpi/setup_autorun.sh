62997e0b2cea99d066ddd384113d19b8439c82fe
topher <iamsomebody@gmail.com>
2021-05-28 20:12:43 +0100

    add dir for rpi setup

---
+++ util/rpi/setup_autorun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo echo '@lxterminal -e /home/pi/efis_to_hud/util/run_demo.sh' >> /etc/xdg/lxsession/LXDE-pi/autostart 
---
+++ util/run_demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/efis_to_hud
+sudo python hud.py -i serial_d100 -e
+

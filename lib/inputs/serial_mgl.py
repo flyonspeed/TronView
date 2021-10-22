4ac4a2684b2e8096f91773e58a739de43cc03b62
dinglewanker <iamsomebody@gmail.com>
2021-10-22 11:41:26 -0700

    add turn rate

--- lib/inputs/serial_mgl.py
+++ lib/inputs/serial_mgl.py
@@ -91,7 +91,7 @@ class serial_mgl(Input):
                             aircraft.roll = BankAngle * 0.1  #
                             if HeadingMag != 0:
                                 aircraft.mag_head = HeadingMag * 0.1
-                            aircraft.TurnRate = TurnRate
+                            aircraft.turn_rate = TurnRate
                             aircraft.slip_skid = (Slip * 0.01 * -1) * 2 # convert to aircraft format -100 to 100.  postive is to left. #LRForce * 0.01 
                             aircraft.vert_G = GForce * 0.01
                             aircraft.msg_count += 1

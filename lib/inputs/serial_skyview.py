3189233b850fabd0392c376130423c2af88e06da
Zap <zap@tr182.com>
2024-12-28 05:53:33 -0700

    Added Skyview message type 2 and 3 decodes to serial_skyview.py & serial_read.py

--- lib/inputs/serial_skyview.py
+++ lib/inputs/serial_skyview.py
@@ -3,7 +3,8 @@
 # Serial input source
 # Skyview
 # 1/23/2019  Topher
-# 11/6/2024  Added IMU data.
+# 11/6/2024  Added IMU data
+# 11/30/2024 Added NAV, Autopilot and EMS data   Zap
 
 from ._input import Input
 from lib import hud_utils
@@ -94,8 +95,8 @@ class serial_skyview(Input):
                     if(isinstance(msg,str)): msg = msg.encode() # if read from file then convert to bytes
                     HH, MM, SS, FF, pitch, roll, HeadingMAG, IAS, PresAlt, TurnRate, LatAccel, VertAccel, AOA, VertSpd, OAT, TAS, Baro, DA, WD, WS, Checksum, CRLF = struct.unpack(
                         "2s2s2s2s4s5s3s4s6s4s3s3s2s4s3s4s3s6s3s2s2s2s", msg
-                    ) 
-                    #print(msg)
+                    )
+                    #print("AHRS Message !1:", msg)
                     aircraft.sys_time_string = "%d:%d:%d"%(int(HH),int(MM),int(SS))
                     self.time_stamp_string = aircraft.sys_time_string
                     self.time_stamp_min = int(MM)
@@ -129,7 +130,7 @@ class serial_skyview(Input):
                     #print("TAS:"+str(TAS))
                     aircraft.tas = Input.cleanInt(self,TAS) * 0.1
                     #print("AOA:"+str(AOA))
-                    if AOA == "XX":
+                    if AOA == b'XX':
                         aircraft.aoa = 0
                     else:
                         aircraft.aoa = Input.cleanInt(self,AOA)
@@ -155,8 +156,6 @@ class serial_skyview(Input):
                         aircraft.wind_speed = 0
                         aircraft.norm_wind_dir = 0 #normalize the wind direction to the airplane heading
                         aircraft.gndspeed = 0
-
-
                     aircraft.msg_count += 1
 
                     if self.output_logFile != None:
@@ -165,19 +164,77 @@ class serial_skyview(Input):
 
                 elif dataType == b'2': #Dynon System message (nav,AP, etc)
                     aircraft.nav.msg_count += 1
-                    #8s     3s   5s      4s     4s    3s     c          c            2s       3s            3s c     c          c    c       c    3s      5s      c            
-                    sysTime,HBug,AltBug, AirBug,VSBug,Course,CDISrcType,CDISourePort,CDIScale,CDIDeflection,GS,APEng,APRollMode,Not1,APPitch,Not2,APRollF,APRollP,APRollSlip= struct.unpack(
-                        "8s3s5s4s4s3scc2s3s3sccccc3s5sc", msg
-                    ) 
+                    msg = self.ser.read(90)
+                    if isinstance(msg, str): msg = msg.encode()  # if read from file then convert to bytes
+                    HH,MM,SS,FF,HBug,AltBug, ASIBug,VSBug,Course,CDISrcType,CDISourePort,CDIScale,CDIDeflection,GS,APEng,APRollMode,Not1,APPitch,Not2,APRollF,APRollP,APRollSlip,APPitchF, APPitchP,APPitchSlip,APYawF,APYawP,APYawSlip,TransponderStatus,TransponderReply,TransponderIdent,TransponderCode,DynonUnused,Checksum,CRLF= struct.unpack(
+                        "2s2s2s2s3s5s4s4s3scc2s3s3sccccc3s5sc3s5sc3s5scccc4s10s2s2s", msg
+                    )
+                    #print("NAV & System Message !2:", msg)
+                    aircraft.sys_time_string = "%d:%d:%d"%(int(HH),int(MM),int(SS))
+                    self.time_stamp_string = aircraft.sys_time_string
+                    self.time_stamp_min = int(MM)
+                    self.time_stamp_sec = int(SS)
 
-                elif dataType == b'3': #Engine data message
-                    aircraft.engine.msg_count += 1 
+                    if HBug != b'XXX': aircraft.nav.HeadBug = Input.cleanInt(self, HBug)
+                    if AltBug != b'XXXXX': aircraft.nav.AltBug = Input.cleanInt(self,AltBug) * 10
+                    if ASIBug != b'XXXX': aircraft.nav.ASIBug = Input.cleanInt(self,ASIBug) / 10
+                    if VSBug != b'XXXX': aircraft.nav.VSBug = Input.cleanInt(self,VSBug) / 10
+                    if CDIDeflection != b'XXX': aircraft.nav.ILSDev = Input.cleanInt(self,CDIDeflection)
+                    if GS != b'XXX': aircraft.nav.GSDev = Input.cleanInt(self,GS)
+                    aircraft.nav.HSISource = Input.cleanInt(self,CDISourePort)
+                    if CDISrcType == b'0': navSourceType = 'GPS'
+                    if CDISrcType == b'1': navSourceType = 'NAV'
+                    if CDISrcType == b'2': navSourceType = 'LOC'
+                    aircraft.nav.SourceDesc = navSourceType + str(Input.cleanInt(self,CDISourePort))
+                    aircraft.nav.GLSHoriz = Input.cleanInt(self,CDIScale) / 10
 
+                    if self.output_logFile != None:
+                        Input.addToLog(self,self.output_logFile,bytes([33,int(dataType),int(dataVer)]))
+                        Input.addToLog(self,self.output_logFile,msg)
+
+                elif dataType == b'3': #Dynon EMS Engine data message
+                    aircraft.engine.msg_count += 1
+                    msg = self.ser.read(222)
+                    if isinstance(msg,str):msg = msg.encode() # if read from file then convert to bytes
+                    HH,MM,SS,FF,OilPress,OilTemp, RPM_L,RPM_R,MAP,FF1,FF2,FP,FL_L,FL_R,Frem,V1,V2,AMPs,Hobbs,Tach,TC1,TC2,TC3,TC4,TC5,TC6,TC7,TC8,TC9,TC10,TC11,TC12,TC13,TC14,GP1,GP2,GP3,GP4,GP5,GP6,GP7,GP8,GP9,GP10,GP11,GP12,GP13,Contacts,Pwr,EGTstate,Checksum,CRLF= struct.unpack(
+                         "2s2s2s2s3s4s4s4s3s3s3s3s3s3s3s3s3s4s5s5s4s4s4s4s4s4s4s4s4s4s4s4s4s4s6s6s6s6s6s6s6s6s6s6s6s6s6s16s3s1s2s2s", msg
+                    )
+                    #print("EMS Message !3:", msg)
+                    aircraft.sys_time_string = "%d:%d:%d"%(int(HH),int(MM),int(SS))
+                    self.time_stamp_string = aircraft.sys_time_string
+                    self.time_stamp_min = int(MM)
+                    self.time_stamp_sec = int(SS)
+
+                    aircraft.engine.OilPress = Input.cleanInt(self,OilPress)
+                    aircraft.engine.OilTemp = Input.cleanInt(self,OilTemp)
+                    aircraft.engine.RPM = Input.cleanInt(self,RPM_L)
+                    aircraft.engine.ManPress = Input.cleanInt(self,MAP) / 10
+                    aircraft.engine.FuelFlow = Input.cleanInt(self,FF1) / 10
+                    aircraft.engine.FuelPress = Input.cleanInt(self,FP) / 10
+                    fuel_level_left  = Input.cleanInt(self, FL_L) / 10
+                    fuel_level_right = Input.cleanInt(self, FL_R) / 10
+                    aircraft.engine.FuelLevels = [fuel_level_left, fuel_level_right, 0, 0]
+                    aircraft.fuel.FuelLevels = [fuel_level_left, fuel_level_right, 0, 0]
+
+                    if TC12 != b'XXXX': aircraft.engine.EGT[0] = round(((Input.cleanInt(self, TC12)) * 1.8) + 32)  # convert from C to F
+                    if TC10 != b'XXXX': aircraft.engine.EGT[1] = round(((Input.cleanInt(self, TC10)) * 1.8) + 32)  # convert from C to F
+                    if TC8 != b'XXXX': aircraft.engine.EGT[2] = round(((Input.cleanInt(self, TC8)) * 1.8) + 32)  # convert from C to F
+                    if TC6 != b'XXXX': aircraft.engine.EGT[3] = round(((Input.cleanInt(self, TC6)) * 1.8) + 32)  # convert from C to F
+                    if TC4 != b'XXXX': aircraft.engine.EGT[4] = round(((Input.cleanInt(self, TC4)) * 1.8) + 32)  # convert from C to F
+                    if TC2 != b'XXXX': aircraft.engine.EGT[5] = round(((Input.cleanInt(self, TC2)) * 1.8) + 32)  # convert from C to F
+
+                    if TC11 != b'XXXX': aircraft.engine.CHT[0] = round(((Input.cleanInt(self, TC11)) * 1.8) + 32)  # convert from C to F
+                    if TC9 != b'XXXX': aircraft.engine.CHT[1] = round(((Input.cleanInt(self, TC9)) * 1.8) + 32)  # convert from C to F
+                    if TC7 != b'XXXX': aircraft.engine.CHT[2] = round(((Input.cleanInt(self, TC7)) * 1.8) + 32)  # convert from C to F
+                    if TC5 != b'XXXX': aircraft.engine.CHT[3] = round(((Input.cleanInt(self, TC5)) * 1.8) + 32)  # convert from C to F
+                    if TC3 != b'XXXX': aircraft.engine.EGT[4] = round(((Input.cleanInt(self, TC3)) * 1.8) + 32)  # convert from C to F
+                    if TC1 != b'XXXX': aircraft.engine.EGT[5] = round(((Input.cleanInt(self, TC1)) * 1.8) + 32)  # convert from C to F
+
+                    if self.output_logFile != None:
+                        Input.addToLog(self,self.output_logFile,bytes([33,int(dataType),int(dataVer)]))
+                        Input.addToLog(self,self.output_logFile,msg)
                 else:
                     aircraft.msg_unknown += 1 # unknown message found.
-
-            else:
-                aircraft.msg_bad += 1 # count this as a bad message
         except ValueError:
             aircraft.msg_bad += 1
             #print("bad:"+str(msg))
@@ -190,13 +247,11 @@ class serial_skyview(Input):
             traceback.print_exc()
             aircraft.errorFoundNeedToExit = True
 
-
         if self.isPlaybackMode:  #if play back mode then add a delay.  Else reading a file is way to fast.
             time.sleep(.05)
         else:
             #pass
             self.ser.flushInput()  # flush the serial after every message else we see delays
-
 
         return aircraft
 

1c3b6589305a73519abc65a34b74c1ae732906c7
midwestpanels <info@midwestpanels.com>
2025-03-22 22:57:35 -0400

    Update serial_g3x.py

--- lib/inputs/serial_g3x.py
+++ lib/inputs/serial_g3x.py
@@ -266,6 +266,7 @@ class serial_g3x(Input):
                             current_time = time.time() # calculate hz.
                             self.imuData.hz = round(1 / (current_time - self.last_read_time), 1)
                             self.last_read_time = current_time
+                            print(current_time)
                         return dataship
                     else:
                         self.airData.msg_bad += 1
